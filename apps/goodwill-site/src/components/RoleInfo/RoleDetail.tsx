65bc497f128df9a477676741b995723340231302
coder0104 <77782156+coder0104@users.noreply.github.com>
2025-02-23 01:43:51 +0900

    career page modify

--- apps/goodwill-site/src/components/RoleInfo/RoleDetail.tsx
+++ apps/goodwill-site/src/components/RoleInfo/RoleDetail.tsx
@@ -1,475 +1,93 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
+import '../../styles/RoleDetail.css';
 
 const RoleDetail = () => {
   return (
-    <div
-      css={css`
-        display: flex;
-        justify-content: space-between;
-        padding: 0 80px;
-        max-width: 80%;
-        margin: 0 auto;
-      `}
-    >
-      {/* 왼쪽 컨텐츠 */}
-      <div
-        css={css`
-          margin-right: 1rem;
-        `}
-      >
-        <div
-          css={css`
-            font-family: Pretendard;
-            font-size: 56px;
-            font-style: normal;
-            font-weight: 600;
-          `}
-        >
-          역할명
-        </div>
-        <div
-          css={css`
-            font-family: Pretendard;
-            font-size: 36px;
-            font-style: normal;
-            font-weight: 600;
-            margin-top: 16px;
-          `}
-        >
-          대제목
-        </div>
-        <div
-          css={css`
-            margin-top: 12px;
-          `}
-        >
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 24px;
-              font-style: normal;
-              font-weight: 700;
-              opacity: 0.75;
-              line-height: 140%; /* 33.6px */
-            `}
-          >
-            굿윌은 이런 인재가 필요합니다.
-          </div>
-          <div
-            css={css`
-              margin-top: 12px;
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-        </div>
-        <div
-          css={css`
-            margin-top: 48px;
-          `}
-        >
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 24px;
-              font-style: normal;
-              font-weight: 700;
-              opacity: 0.75;
-              line-height: 140%; /* 33.6px */
-            `}
-          >
-            UXUI Designer는...
-          </div>
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-        </div>
-        <div
-          css={css`
-            margin-top: 46px;
-          `}
-        >
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 24px;
-              font-style: normal;
-              font-weight: 700;
-              opacity: 0.75;
-              line-height: 140%; /* 33.6px */
-            `}
-          >
-            소제목
-          </div>
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-          <div
-            css={css`
-              font-family: Pretendard;
-              font-size: 18px;
-              font-style: normal;
-              font-weight: 500;
-              opacity: 0.75;
-              line-height: 160%; /* 28.8px */
-            `}
-          >
-            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해
-            주세요.
-          </div>
-        </div>
-        <div
-          css={css`
-            margin-top: 48px;
-          `}
-        >
-          <div
-            css={css`
-              display: flex;
-              align-items: center
-              font-family: Pretendard;
-              font-size: 24px;
-              font-style: normal;
-              font-weight: 700;
-              opacity: 0.75;
-              line-height: 140%; /* 33.6px */
-            `}
-          >
-            GOODWILL 합류 과정
-            <img
-              src="./icons/CaretRight.svg"
-              alt="My Icon"
-              css={css`
-                display: inline-block; /* 아이콘을 블록처럼 다룰 수 있도록 설정 */
-                vertical-align: middle; /* 텍스트와 중간 정렬 */
+    <div>
+    <div className="role-detail">
+      <div className="left-content">
+        <div className="role-name">역할명</div>
+        <div className="main-title">대제목</div>
 
-                margin-left: 5px;
-              `}
-            />
+        <div className="requirement">
+          <div className="subtitle">굿윌은 이런 인재가 필요합니다.</div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
           </div>
-          <div
-            css={css`
-              margin-top: 8px;
-              display: flex;
-            `}
-          >
-            <div
-              css={css`
-                font-family: Pretendard;
-                font-size: 18px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 160%;
-              `}
-            >
-              지원서 접수
-            </div>
-            <img
-              src="./icons/CaretRight.svg"
-              css={css`
-                margin-left: 6px;
-                margin-right: 6px;
-                width: 16px;
-                height: 12px;
-                display: inline-block;
-                vertical-align: middle;
-                margin-top: 6px;
-              `}
-            />
-            <div
-              css={css`
-                font-family: Pretendard;
-                font-size: 18px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 160%;
-              `}
-            >
-              1차 코딩 테스트
-            </div>
-            <img
-              src="./icons/CaretRight.svg"
-              css={css`
-                margin-left: 6px;
-                margin-right: 6px;
-                width: 16px;
-                height: 12px;
-                display: inline-block;
-                vertical-align: middle;
-                margin-top: 6px;
-              `}
-            />
-            <div
-              css={css`
-                font-family: Pretendard;
-                font-size: 18px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 160%;
-              `}
-            >
-              2차 직무 인터뷰
-            </div>
-            <img
-              src="./icons/CaretRight.svg"
-              css={css`
-                margin-left: 6px;
-                margin-right: 6px;
-                width: 16px;
-                height: 12px;
-                display: inline-block;
-                vertical-align: middle;
-                margin-top: 6px;
-              `}
-            />
-            <div
-              css={css`
-                font-family: Pretendard;
-                font-size: 18px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 160%;
-              `}
-            >
-              3차 문화적합성 인터뷰
-            </div>
-            <img
-              src="./icons/CaretRight.svg"
-              css={css`
-                margin-left: 6px;
-                margin-right: 6px;
-                width: 16px;
-                height: 12px;
-                display: inline-block;
-                vertical-align: middle;
-                margin-top: 6px;
-              `}
-            />
-            <div
-              css={css`
-                font-family: Pretendard;
-                font-size: 18px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 160%;
-              `}
-            >
-              레퍼런스 체크 및 GOODWILL 입사 여부 재확인
-            </div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
           </div>
         </div>
+
+        <div className="uxui-designer">
+          <div className="subtitle">UXUI Designer는...</div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
+          </div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
+          </div>
+        </div>
+
+        <div className="sub-title-section">
+          <div className="subtitle">소제목</div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
+          </div>
+          <div className="description">
+            &middot; 내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.
+          </div>
+        </div>
+
       </div>
-      {/* 오른쪽 카드 */}
-      <div
-        css={css`
-          flex-shrink: 0;
-          margin-left: auto;
-        `}
-      >
-        <div
-          css={css`
-            border: 1px solid #d9d9d9;
-            width: 355px;
-            height: 431px;
-            border-radius: var(--Spacing-spacing-8, 8px);
-          `}
-        >
-          <div
-            css={css`
-              padding: 10px 20px 16px 20px;
-            `}
-          >
-            <div>
-              <div
-                css={css`
-                  display: flex;
-                  border-bottom: 1px solid #d9d9d9;
-                  padding: 17px 0 17px 0;
-                `}
-              >
-                <div
-                  css={css`
-                    margin: 0 64px 0 5px;
-                    color: #9d9d9d;
-                    font-family: Pretendard;
-                    font-size: 16px;
-                    font-style: normal;
-                    font-weight: 500;
-                    line-height: 24px;
-                    letter-spacing: -0.048px;
-                  `}
-                >
-                  직군
-                </div>
-                <div>Design</div>
-              </div>
+        
+
+      <div className="right-card">
+        <div className="card">
+          <div className="card-content">
+            <div className="card-item">
+              <div className="card-label">직군</div>
+              <div className="card-value">Design</div>
             </div>
-            <div>
-              <div
-                css={css`
-                  display: flex;
-                  border-bottom: 1px solid #d9d9d9;
-                  padding: 17px 0 17px 0;
-                `}
-              >
-                <div
-                  css={css`
-                    margin: 0 36px 0 5px;
-                    color: #9d9d9d;
-                    font-family: Pretendard;
-                    font-size: 16px;
-                    font-style: normal;
-                    font-weight: 500;
-                    line-height: 24px;
-                    letter-spacing: -0.048px;
-                  `}
-                >
-                  경력사항
-                </div>
-                <div>경력 1년 이상</div>
-              </div>
+            <div className="card-item">
+              <div className="card-label" css={css`margin: 0 36px 0px 5px;`}>경력사항</div>
+              <div className="card-value" >경력 1년 이상</div>
             </div>
-            <div>
-              <div
-                css={css`
-                  display: flex;
-                  border-bottom: 1px solid #d9d9d9;
-                  padding: 17px 0 17px 0;
-                `}
-              >
-                <div
-                  css={css`
-                    margin: 0 36px 0 5px;
-                    color: #9d9d9d;
-                    font-family: Pretendard;
-                    font-size: 16px;
-                    font-style: normal;
-                    font-weight: 500;
-                    line-height: 24px;
-                    letter-spacing: -0.048px;
-                  `}
-                >
-                  고용형태
-                </div>
-                <div>정규직</div>
-              </div>
+            <div className="card-item">
+              <div className="card-label" css={css`margin: 0 36px 0px 5px;`}>고용형태</div>
+              <div className="card-value">정규직</div>
             </div>
-            <div>
-              <div
-                css={css`
-                  display: flex;
-                  padding: 17px 0 0 0;
-                `}
-              >
-                <div
-                  css={css`
-                    margin: 0 50px 0 5px;
-                    color: #9d9d9d;
-                    font-family: Pretendard;
-                    font-size: 16px;
-                    font-style: normal;
-                    font-weight: 500;
-                    line-height: 24px;
-                    letter-spacing: -0.048px;
-                  `}
-                >
-                  사무실
-                </div>
-                <div>
-                  대전대신고등학교 <br /> 대전광역시 서구 오량1길 98
-                </div>
-              </div>
+            <div className="card-item-last">
+              <div className="card-label" css={css`margin: 0 50px 0px 5px;`}>사무실</div>
+              <div className="card-value">대전대신고등학교 <br /> 대전광역시 서구 오량1길 98</div>
             </div>
-            <div
-              css={css`
-                margin-top: 16px;
-                width: 315px;
-                height: 147px;
-                border-radius: var(--Spacing-spacing-4, 4px);
-                background: #d9d9d9;
-              `}
-            ></div>
+            <div className="card-image"></div>
           </div>
         </div>
-        <div
-          css={css`
-            box-sizing: border-box; /* padding과 border를 width와 height에 포함 */
-            margin-top: 20px;
-            display: flex;
-            width: 355px;
-            height: 50px;
-            padding: 18px 20px;
-            justify-content: center;
-            align-items: center;
-            flex-shrink: 0;
-            border-radius: var(--Spacing-spacing-8, 8px);
-            background: var(--brand-primary-primary-800, #05308c);
-            color: var(--gray-scale-white-0, #fff);
-            font-family: Pretendard;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 300;
-            line-height: 140%;
-          `}
-        >
-          지원하기
-        </div>
+
+        <div className="apply-button">지원하기</div>
       </div>
+      
     </div>
+    <div className="process_toworkwith">
+    <div className="process-title">
+      GOODWILL 합류 과정
+      <img src="./icons/CaretRight.svg" alt="My Icon" className="caret-icon" />
+    </div>
+    <div className="process-steps">
+      <div className="step">지원서 접수</div>
+      <img src="./icons/CaretRight.svg" alt="My Icon" className="caret-icon" />
+      <div className="step">1차 코딩 테스트</div>
+      <img src="./icons/CaretRight.svg" alt="My Icon" className="caret-icon" />
+      <div className="step">2차 직무 인터뷰</div>
+      <img src="./icons/CaretRight.svg" alt="My Icon" className="caret-icon" />
+      <div className="step">3차 문화적합성 인터뷰</div>
+      <img src="./icons/CaretRight.svg" alt="My Icon" className="caret-icon" />
+      <div className="step">레퍼런스 체크 및 GOODWILL 입사 여부 재확인</div>
+    </div>
+  </div>
+  </div>
   );
 };
 
