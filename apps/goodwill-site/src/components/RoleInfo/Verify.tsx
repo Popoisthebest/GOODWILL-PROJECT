8f8c9d254ddda39e7d628d22dd4b2b95e75b3ae9
coder0104 <77782156+coder0104@users.noreply.github.com>
2025-02-24 01:33:50 +0900

    '논술 평가 수험자 인증 절차 신원확인/시작하기' 페이지 프론트 구현

---
+++ apps/goodwill-site/src/components/RoleInfo/Verify.tsx
@@ -0,0 +1,21 @@
+import '../../styles/Verify.css'
+
+const Verify = () => {
+  return (
+    <>
+      <div className="main_box">
+        <div className="first_child">
+          <div className="sub_text">GOODWILL Corp.</div>
+          <div className="sub_text">논술 평가 수험자 인증 절차</div>
+        </div>
+        <div className="sec_child">
+          <div className="sec_child_text">신원확인</div>
+          <input className='sec_child_ipb' placeholder='수험번호' type="text" />
+        </div>
+          <button className="start_btn">시작하기</button>
+      </div>
+    </>
+  )
+}
+
+export default Verify
--- apps/goodwill-site/src/pages/Role.tsx
+++ apps/goodwill-site/src/pages/Role.tsx
@@ -1,5 +1,6 @@
 import EssayEval from "../components/RoleInfo/EssayEval";
+import Verify from "../components/RoleInfo/Verify";
 
-const Role = () => <EssayEval />
+const Role = () => <Verify />
 
 export default Role
