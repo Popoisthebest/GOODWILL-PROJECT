c292111f65e2352ba03ff4e44c59ffba4d5ed160
Popo <147984101+Popoisthebest@users.noreply.github.com>
2025-03-10 19:03:45 +0900

    add(goodwill-site): app-fixed

--- apps/goodwill-site/src/App.tsx
+++ apps/goodwill-site/src/App.tsx
@@ -2,19 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RoleSelect from "./pages/RoleSelect.tsx";
 import RoleDetail from "./components/RoleInfo/RoleDetail.tsx";
 import ApplicationFormPage from "./pages/ApplicationForm.tsx";
-// import RoleDetailBackEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailBackendEngineer.tsx";
-// import RoleDetailFrontEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFrontendEngineer.tsx";
-// import RoleDetailFullStackEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFullstackEngineer.tsx";
 // import TeamCulture from "./pages/TeamCulture.tsx";
 import Announcement from "./pages/Announcement.tsx";
 import NoticeDetail from "./pages/NoticeDetail.tsx";
 // import Article from "./pages/Article.tsx";
 // import Project from "./pages/Project.tsx";
-// import Main from "./pages/Main.tsx";
 import SubmissionFinished from "./pages/SubmissionFinished.tsx";
 import MainAbout from "./pages/MainAbout.tsx";
 import Main from "./pages/Main.tsx";
-// import About from "./components/About/About.tsx";
 
 function App() {
   return (
@@ -26,18 +21,7 @@ function App() {
         <Route path="/about" element={<MainAbout />} />
         <Route path="/write-application" element={<ApplicationFormPage />} />
         <Route path="/submission-finished" element={<SubmissionFinished />} />
-        {/*<Route*/}
-        {/*  path="/Back-End Engineer(BE)"*/}
-        {/*  element={<RoleDetailBackEndEngineer />}*/}
-        {/*/>*/}
-        {/*<Route*/}
-        {/*  path="/Front-End Engineer(FE)"*/}
-        {/*  element={<RoleDetailFrontEndEngineer />}*/}
-        {/*/>*/}
-        {/*<Route*/}
-        {/*  path="/Full-Stack Engineer(FS)"*/}
-        {/*  element={<RoleDetailFullStackEngineer />}*/}
-        {/*/>*/}
+
         {/*<Route path="/team-and-culture" element={<TeamCulture />} />*/}
         {/*<Route path="/project" element={<Project />} />*/}
         <Route path="/announcement" element={<Announcement />} />
