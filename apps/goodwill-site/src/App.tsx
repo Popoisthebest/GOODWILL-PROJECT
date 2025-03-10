d63655498dc0af4b8faa09b48a509de0bd833841
Popo <147984101+Popoisthebest@users.noreply.github.com>
2025-03-10 17:00:37 +0900

    add(goodwill-site): career-added

--- apps/goodwill-site/src/App.tsx
+++ apps/goodwill-site/src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RoleSelect from "./pages/RoleSelect.tsx";
 import ApplicationFormPage from "./pages/ApplicationForm.tsx";
-import RoleDetailBackEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailBackendEngineer.tsx";
-import RoleDetailFrontEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFrontendEngineer.tsx";
-import RoleDetailFullStackEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFullstackEngineer.tsx";
 import TeamCulture from "./pages/TeamCulture.tsx";
 import Announcement from "./pages/Announcement.tsx";
 import NoticeDetail from "./pages/NoticeDetail.tsx";
@@ -12,6 +9,7 @@ import Project from "./pages/Project.tsx";
 import Main from "./pages/Main.tsx";
 import SubmissionFinished from "./pages/SubmissionFinished.tsx";
 import MainAbout from "./pages/MainAbout.tsx";
+import RoleDetail from "./components/RoleInfo/RoleDetail.tsx";
 
 function App() {
   return (
@@ -21,21 +19,8 @@ function App() {
         <Route path="/career" element={<RoleSelect />} />
         <Route path="/about" element={<MainAbout />} />
         <Route path="/:roleName" element={<RoleDetail />} />
-        {/*<Route path="/about" element={<About />} />*/}
         <Route path="/write-application" element={<ApplicationFormPage />} />
         <Route path="/submission-finished" element={<SubmissionFinished />} />
-        <Route
-          path="/Back-End Engineer(BE)"
-          element={<RoleDetailBackEndEngineer />}
-        />
-        <Route
-          path="/Front-End Engineer(FE)"
-          element={<RoleDetailFrontEndEngineer />}
-        />
-        <Route
-          path="/Full-Stack Engineer(FS)"
-          element={<RoleDetailFullStackEngineer />}
-        />
         <Route path="/team-and-culture" element={<TeamCulture />} />
         <Route path="/project" element={<Project />} />
         <Route path="/announcement" element={<Announcement />} />
