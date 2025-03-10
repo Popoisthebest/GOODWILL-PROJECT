962c8b15236fbdf3b58f5ab27db5e2317c264f8a
Popo <147984101+Popoisthebest@users.noreply.github.com>
2025-03-10 09:56:29 +0900

    add(goodwill-site): only-career

--- apps/goodwill-site/src/App.tsx
+++ apps/goodwill-site/src/App.tsx
@@ -1,47 +1,47 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RoleSelect from "./pages/RoleSelect.tsx";
-import ApplicationFormPage from "./pages/ApplicationForm.tsx";
-import RoleDetailBackEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailBackendEngineer.tsx";
-import RoleDetailFrontEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFrontendEngineer.tsx";
-import RoleDetailFullStackEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFullstackEngineer.tsx";
-import TeamCulture from "./pages/TeamCulture.tsx";
-import Announcement from "./pages/Announcement.tsx";
-import NoticeDetail from "./pages/NoticeDetail.tsx";
-import Article from "./pages/Article.tsx";
-import Project from "./pages/Project.tsx";
-import Main from "./pages/Main.tsx";
-import SubmissionFinished from "./pages/SubmissionFinished.tsx";
-import About from "./components/About/About.tsx";
+// import ApplicationFormPage from "./pages/ApplicationForm.tsx";
+// import RoleDetailBackEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailBackendEngineer.tsx";
+// import RoleDetailFrontEndEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFrontendEngineer.tsx";
+// import RoleDetailFullStackEngineer from "./components/RoleInfo/RoleDetails/RoleDetailFullstackEngineer.tsx";
+// import TeamCulture from "./pages/TeamCulture.tsx";
+// import Announcement from "./pages/Announcement.tsx";
+// import NoticeDetail from "./pages/NoticeDetail.tsx";
+// import Article from "./pages/Article.tsx";
+// import Project from "./pages/Project.tsx";
+// import Main from "./pages/Main.tsx";
+// import SubmissionFinished from "./pages/SubmissionFinished.tsx";
+// import About from "./components/About/About.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<RoleSelect />} />
         <Route path="/career" element={<RoleSelect />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/write-application" element={<ApplicationFormPage />} />
-        <Route path="/submission-finished" element={<SubmissionFinished />} />
-        <Route
-          path="/Back-End Engineer(BE)"
-          element={<RoleDetailBackEndEngineer />}
-        />
-        <Route
-          path="/Front-End Engineer(FE)"
-          element={<RoleDetailFrontEndEngineer />}
-        />
-        <Route
-          path="/Full-Stack Engineer(FS)"
-          element={<RoleDetailFullStackEngineer />}
-        />
-        <Route path="/team-and-culture" element={<TeamCulture />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/announcement" element={<Announcement />} />
-        <Route
-          path="/notice/:category/:title/:date/:views/:content"
-          element={<NoticeDetail />}
-        />
-        <Route path="/article" element={<Article />} />
+        {/*<Route path="/about" element={<About />} />*/}
+        {/*<Route path="/write-application" element={<ApplicationFormPage />} />*/}
+        {/*<Route path="/submission-finished" element={<SubmissionFinished />} />*/}
+        {/*<Route*/}
+        {/*  path="/Back-End Engineer(BE)"*/}
+        {/*  element={<RoleDetailBackEndEngineer />}*/}
+        {/*/>*/}
+        {/*<Route*/}
+        {/*  path="/Front-End Engineer(FE)"*/}
+        {/*  element={<RoleDetailFrontEndEngineer />}*/}
+        {/*/>*/}
+        {/*<Route*/}
+        {/*  path="/Full-Stack Engineer(FS)"*/}
+        {/*  element={<RoleDetailFullStackEngineer />}*/}
+        {/*/>*/}
+        {/*<Route path="/team-and-culture" element={<TeamCulture />} />*/}
+        {/*<Route path="/project" element={<Project />} />*/}
+        {/*<Route path="/announcement" element={<Announcement />} />*/}
+        {/*<Route*/}
+        {/*  path="/notice/:category/:title/:date/:views/:content"*/}
+        {/*  element={<NoticeDetail />}*/}
+        {/*/>*/}
+        {/*<Route path="/article" element={<Article />} />*/}
       </Routes>
     </Router>
   );
