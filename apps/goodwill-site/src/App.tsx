c9d0ed4bc16747a260e7091a738263a736ee3b65
Popo <147984101+Popoisthebest@users.noreply.github.com>
2025-03-12 19:09:29 +0900

    fix(goodwill-site): project-page-added

--- apps/goodwill-site/src/App.tsx
+++ apps/goodwill-site/src/App.tsx
@@ -6,7 +6,7 @@ import ApplicationFormPage from "./pages/ApplicationForm.tsx";
 import Announcement from "./pages/Announcement.tsx";
 import NoticeDetail from "./pages/NoticeDetail.tsx";
 // import Article from "./pages/Article.tsx";
-// import Project from "./pages/Project.tsx";
+import Project from "./pages/Project.tsx";
 import SubmissionFinished from "./pages/SubmissionFinished.tsx";
 import MainAbout from "./pages/About.tsx";
 import Main from "./pages/Main.tsx";
@@ -48,7 +48,7 @@ function App() {
         <Route path="/submission-finished" element={<SubmissionFinished />} />
 
         {/*<Route path="/team-and-culture" element={<TeamCulture />} />*/}
-        {/*<Route path="/project" element={<Project />} />*/}
+        <Route path="/project" element={<Project />} />
         <Route path="/announcement" element={<Announcement />} />
         <Route
           path="/notice/:category/:title/:date/:views/:content"
